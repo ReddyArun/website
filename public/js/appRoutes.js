e5c3d5298534c6c10566b480a0f293b4eda4bbba
Arun Kumar <reddyarunkumar@gmail.com>
2016-09-22 21:58:34 -0600

    converting to angular js

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -20,6 +20,62 @@ angular.module('appRoutes', [])
                     url: '/contact',
                     templateUrl: 'contact.html'
                 })
+                .state('portfolio-1', {
+                    url: '/portfolio-1',
+                    templateUrl: 'portfolio/portfolio-1-col.html'
+                })
+                .state('portfolio-2', {
+                    url: '/portfolio-2',
+                    templateUrl: 'portfolio/portfolio-2-col.html'
+                })
+                .state('portfolio-3', {
+                    url: '/portfolio-3',
+                    templateUrl: 'portfolio/portfolio-3-col.html'
+                })
+                .state('portfolio-4', {
+                    url: '/portfolio-4',
+                    templateUrl: 'portfolio/portfolio-4-col.html'
+                })
+                .state('portfolio-item', {
+                    url: '/portfolio-item',
+                    templateUrl: 'portfolio/portfolio-item.html'
+                })
+                .state('blog-home-1', {
+                    url: '/blog-home-1',
+                    templateUrl: 'blog/blog-home-1.html'
+                })
+                .state('blog-home-2', {
+                    url: '/blog-home-2',
+                    templateUrl: 'blog/blog-home-2.html'
+                })
+                .state('blog-post', {
+                    url: '/blog-post',
+                    templateUrl: 'blog/blog-post.html'
+                })
+                .state('full-width', {
+                    url: '/full-width',
+                    templateUrl: 'otherpage/full-width.html'
+                })
+                .state('404link', {
+                    url: '/404',
+                    templateUrl: 'otherpage/404.html'
+                })
+                .state('faq', {
+                    url: '/faq',
+                    templateUrl: 'otherpage/faq.html'
+                })
+                .state('fullwidth', {
+                    url: '/full-width',
+                    templateUrl: 'otherpage/full-width.html'
+                })
+                .state('pricing', {
+                    url: '/pricing',
+                    templateUrl: 'otherpage/pricing.html'
+                })
+                .state('sidebar', {
+                    url: '/sidebar',
+                    templateUrl: 'otherpage/sidebar.html'
+                })
         }
     ]
     );
